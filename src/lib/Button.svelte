247641dbb356ea6c4089b53661b7a6bb0a6b899c
Thibaud BRAULT <thibaud.brault@gide.net>
2024-02-23 10:20:03 +0100

    :sparkles: add tailwind + config tools

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
+	export let addTodo: (text: string) => void;
+	export let text: string;
 
-    export let addTodo: (text: string) => void
-    export let text: string
-
-    const handleClick = () => {
-        addTodo(text)
-    }
-
+	const handleClick = () => {
+		addTodo(text);
+	};
 </script>
 
 <button on:click={handleClick}>This is a button</button>
--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-     export let placeholder: string
-     export let value: string
+	export let placeholder: string;
+	export let value: string;
 </script>
 
-<input type="text" placeholder={placeholder} bind:value={value} />
+<input type="text" {placeholder} bind:value />
