3a1e7a0b992f64d834654cb6fcda83f2112bb10d
Thibaud BRAULT <thibaud.brault@gide.net>
2024-02-28 17:09:41 +0100

    :fire: clean page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,8 @@
 <script lang="ts">
-	import { Button, Input } from '$lib';
-
-	let text: string = 'Hello World!';
-	let todos: string[] = [];
-
-	const addTodo = (text: string) => {
-		todos.push(text);
-		todos = todos;
-	};
-
-	const deleteTodo = (i: number) => {
-		todos.splice(i, 1);
-		todos = todos;
-	};
+	import { Button } from '$components';
 </script>
 
-<main>
-	<h1>Welcome to SvelteKit</h1>
-	<ul>
-		{#each todos as todo, i}
-			<li>
-				<p>{todo}</p>
-				<button on:click={() => deleteTodo(i)}>X</button>
-			</li>
-		{/each}
-	</ul>
-	<p>{text}</p>
-	<Input placeholder={'New todo'} bind:value={text} />
-	<Button {addTodo} {text} />
-</main>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<section class="p-4">
+	<h2>Home</h2>
+	<Button intent="primary" weight="semibold">Hello World!</Button>
+</section>
