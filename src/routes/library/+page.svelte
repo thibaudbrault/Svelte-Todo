338c47fe582fffa8070584df400f73554708052d
Thibaud BRAULT <thibaud.brault@gide.net>
2024-02-28 17:06:38 +0100

    :sparkles: create library page

---
+++ src/routes/library/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Card } from '$components';
+
+	const cards = [
+		{
+			title: 'Hello',
+			desc: 'Hi',
+		},
+		{
+			title: 'Goodbye',
+			desc: 'Bye',
+		},
+	];
+</script>
+
+<section class="space-y-4 p-4">
+	<h2 class="text-4xl font-bold">Library</h2>
+	<div class="flex items-center gap-2">
+		{#each cards as card}
+			<Card title={card.title} desc={card.desc} />
+		{/each}
+	</div>
+</section>
