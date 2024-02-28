fd9ba968cd05039a203006c9a24a5c68ea94c039
Thibaud BRAULT <thibaud.brault@gide.net>
2024-02-28 17:09:05 +0100

    :art: create card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title: string;
+	export let desc: string;
+</script>
+
+<div class="rounded-md bg-neutral-600/50 p-4">
+	<h3 class="text-xl font-semibold">{title}</h3>
+	<p>{desc}</p>
+</div>
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Card } from './Card.svelte';
+export { default as Button } from './Button.svelte';
