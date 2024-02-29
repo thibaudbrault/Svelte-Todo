16cc76222cc11739d7ff14322c49d3811a2dd0e5
Thibaud Brault <thibaud.brault@protonmail.com>
2024-02-29 19:49:18 +0100

    :clown_face: test audio player

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -1,23 +1,44 @@
 <script lang="ts">
 	import { Card } from '$components';
 
-	const cards = [
+	let audio: HTMLAudioElement;
+	let isPaused: boolean = false;
+	const albums = [
 		{
-			title: 'Hello',
+			title: 'HeartGold - SoulSilver',
 			desc: 'Hi',
+			alt: 'HeartGold - SoulSilver cover',
+			cover: '/cover.png',
 		},
-		{
-			title: 'Goodbye',
-			desc: 'Bye',
-		},
+		// {
+		// 	title: 'Goodbye',
+		// 	desc: 'Bye',
+		// },
 	];
+
+	const playPause = () => {
+		isPaused = !isPaused;
+		if (isPaused) {
+			audio.play();
+		} else {
+			audio.pause();
+		}
+	};
 </script>
 
 <section class="space-y-4 p-4">
 	<h2 class="text-4xl font-bold">Library</h2>
-	<div class="flex items-center gap-2">
-		{#each cards as card}
-			<Card title={card.title} desc={card.desc} />
+	<div class=" grid grid-cols-4 gap-6">
+		{#each albums as album}
+			<Card
+				title={album.title}
+				desc={album.desc}
+				alt={album.alt}
+				cover={album.cover}
+			/>
 		{/each}
 	</div>
+	<p>{isPaused}</p>
+	<button on:click={playPause}>{isPaused ? 'Pause' : 'Play'}</button>
+	<audio controls src="" bind:this={audio} />
 </section>
