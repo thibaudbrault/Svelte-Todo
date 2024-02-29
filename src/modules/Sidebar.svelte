910104808d7b572f9ee1afb8d5fba7847069cd2e
Thibaud Brault <thibaud.brault@protonmail.com>
2024-02-29 19:43:00 +0100

    :lipstick: add new links to sidebar

--- src/modules/Sidebar.svelte
+++ src/modules/Sidebar.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
 </script>
 
-<nav class="h-screen space-y-4 border-r border-r-neutral-700 px-8 py-4">
-	<h1 class="tetx-3xl font-semibold">SvelteKit</h1>
-	<ul class="flex flex-col gap-2">
-		<li>
-			<a href="/">Home</a>
-		</li>
-		<li>
-			<a href="/library">Library</a>
-		</li>
-	</ul>
+<nav
+	class="flex h-screen flex-col justify-between space-y-4 border-r border-r-neutral-700 bg-neutral-800 px-12 py-4"
+>
+	<div>
+		<h1 class="text-3xl font-semibold">SvelteKit</h1>
+		<ul class="flex flex-col gap-2">
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/library">Library</a>
+			</li>
+			<li>
+				<a href="/favorites">Favorites</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<ul>
+			<li>
+				<a href="/profile">Profile</a>
+			</li>
+			<li>
+				<a href="/">Sign Out</a>
+			</li>
+		</ul>
+	</div>
 </nav>
