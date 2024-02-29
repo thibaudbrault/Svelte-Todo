8e6934561bff5ef505c52c7facea3c0c190e5681
Thibaud Brault <thibaud.brault@protonmail.com>
2024-02-29 19:42:19 +0100

    :lipstick: add image in card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	export let title: string;
 	export let desc: string;
+	export let alt: string;
+	export let cover: string;
 </script>
 
 <div class="rounded-md bg-neutral-600/50 p-4">
-	<h3 class="text-xl font-semibold">{title}</h3>
+	<img {alt} src={cover} class="rounded-md" />
+	<h3 class="text-center font-semibold">{title}</h3>
 	<p>{desc}</p>
 </div>
