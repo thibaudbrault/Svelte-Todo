62f0147aca1318fc09e3c3a8c18383acf1a45220
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-04 16:18:20 +0100

    :goal_net: return fail when response is null

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,10 +1,14 @@
 import { albums, db, musics } from '$lib/db';
 import { eq } from 'drizzle-orm';
 import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const slug = params.slug;
 	const response = await db.select().from(albums).where(eq(albums.slug, slug));
+	if (!response) {
+		return fail(500, { message: 'Could not find albums' });
+	}
 	const { id, name, cover } = response[0];
 	const result = await db
 		.select()
