aa64b083a7ebf6e7a9b6e7900835cc7d12bf6d8a
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-11 16:58:48 +0100

    :safety_vest: add validation for creating an album

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,4 +1,10 @@
 import { z } from 'zod';
+
+export const createAlbumSchema = z.object({
+	name: z.string().min(1, { message: 'Album name is required' }),
+	cover: z.any(),
+});
+
 export const createSingleMusicSchema = z.object({
 	title: z.string().min(1, { message: 'Title is required' }),
 	duration: z
@@ -13,5 +19,6 @@ export const createMultipleMusicSchema = z.object({
 	tracks: z.any(),
 });
 
+export type CreateAlbum = typeof createAlbumSchema;
 export type CreateSingleMusic = typeof createSingleMusicSchema;
 export type CreateMultipleMusic = typeof createMultipleMusicSchema;
