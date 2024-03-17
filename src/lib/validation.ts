84156500f30cd4b9ca8a4c5411bb4689a558817d
Thibaud Brault <thibaud.brault@protonmail.com>
2024-03-17 18:49:32 +0100

    :safety_vest: update validation schemas

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -7,15 +7,11 @@ export const createAlbumSchema = z.object({
 
 export const createSingleMusicSchema = z.object({
 	title: z.string().min(1, { message: 'Title is required' }),
-	duration: z
-		.number()
-		.min(0, { message: 'Duration must be a positive number' }),
 	number: z.number().min(1, { message: 'Number must be 1 or more' }),
 	track: z.custom<File>((val) => val instanceof File, 'Upload a file'),
 });
 
 export const createMultipleMusicSchema = z.object({
-	number: z.number().min(1, { message: 'Number must be 1 or more' }),
 	tracks: z.custom<File>((val) => val instanceof File, 'Upload a file').array(),
 });
 
