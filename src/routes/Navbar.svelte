54526dd25ee94397af055ad564fefca53dd6b84c
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-19 17:23:18 +0100

    :lipstick: update profile and log out buttons styles

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -2,7 +2,6 @@
 	import { page } from '$app/stores';
 	import { Button, Search } from '$components';
 	import { signIn, signOut } from '@auth/sveltekit/client';
-	import { LogOut, User } from 'lucide-svelte';
 </script>
 
 <nav class="col-span-6 row-start-1 grid grid-cols-6 items-center bg-gray-1 p-4">
@@ -10,29 +9,33 @@
 	<div class="col-span-3 col-start-2 w-full justify-self-start">
 		<Search placeholder="Search by albums or songs" />
 	</div>
-	<div class="col-span-2 gap-4 justify-self-end bg-green-500 px-4">
-		<ul class="flex flex-col gap-4">
-			{#if $page.data.session}
-				<li>
-					<a href="/profile">
-						<User class="text-yellow-12" />
-					</a>
-				</li>
-				<li>
-					<button on:click={() => signOut()}>
-						<LogOut class="text-yellow-12" />
-					</button>
-				</li>
-			{:else}
-				<li>
-					<Button
-						rounded="full"
-						size="large"
-						weight="semibold"
-						on:click={() => signIn('github')}>Log In</Button
-					>
-				</li>
-			{/if}
-		</ul>
-	</div>
+	<ul class="col-span-2 flex items-center gap-2 justify-self-end px-4">
+		{#if $page.data.session}
+			<li>
+				<Button rounded="full" size="large" weight="semibold">Profile</Button>
+			</li>
+			<li>
+				<Button
+					intent="secondary"
+					rounded="full"
+					size="large"
+					weight="semibold"
+					on:click={() => signOut()}
+				>
+					Log Out
+				</Button>
+			</li>
+		{:else}
+			<li>
+				<Button
+					rounded="full"
+					size="large"
+					weight="semibold"
+					on:click={() => signIn('github')}
+				>
+					Log In
+				</Button>
+			</li>
+		{/if}
+	</ul>
 </nav>
