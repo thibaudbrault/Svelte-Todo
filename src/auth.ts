ea2375337a9a96f46876f686c341df3d167ca8e0
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-21 12:47:02 +0100

    :sparkles: add user to db when logging in

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,13 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import google from '@auth/sveltekit/providers/google';
+import { db } from '$lib/db';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [
 		google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET }),
 	],
+	adapter: DrizzleAdapter(db),
 	trustHost: true,
 });
