7ffa7c95dd3c5c51cae48622aede43edec5f0c81
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-25 17:05:50 +0100

    :fire: clean library page

--- src/routes/library/+page.server.ts
+++ src/routes/library/+page.server.ts
@@ -1,9 +1,8 @@
-import { db, albums } from '$lib/db';
-import type { PageServerLoad } from '../$types';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-	const result = await db.select().from(albums);
-	return {
-		result,
-	};
+export const load: PageServerLoad = async (event) => {
+	const session = await event.locals.auth();
+	if (!session?.user) throw redirect(303, 'auth/signin');
+	return {};
 };
--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -1,18 +1,46 @@
 <script lang="ts">
-	import { Card } from '$components';
+	import { Tabs } from 'bits-ui';
 
-	export let data;
-	let albums = data.result;
+	const tabs = [
+		{
+			value: 'playlists',
+			label: 'Playlists',
+		},
+		{
+			value: 'albums',
+			label: 'Albums',
+		},
+		{
+			value: 'artists',
+			label: 'Artists',
+		},
+	];
 </script>
 
-<h2 class="text-4xl font-bold">Library</h2>
-<div class="flex flex-wrap gap-6">
-	{#each albums as album}
-		<Card
-			title={album.name}
-			alt={album.name}
-			cover={album.cover}
-			link={`/album/${album.slug}`}
-		/>
-	{/each}
-</div>
+<Tabs.Root value="playlists" class="space-y-4">
+	<div class="flex items-center gap-4">
+		<h2 class="text-4xl font-bold">Library</h2>
+		<Tabs.List
+			class="mx-auto flex w-fit items-center justify-center gap-4 rounded-md bg-gray-12 px-4 py-2 text-gray-1"
+		>
+			{#each tabs as tab}
+				<Tabs.Trigger
+					value={tab.value}
+					class="rounded-md px-2 py-1 text-xl font-semibold lowercase data-[state=active]:bg-gray-3 data-[state=active]:text-gray-12"
+					style="font-variant: small-caps;"
+				>
+					{tab.label}
+				</Tabs.Trigger>
+			{/each}
+		</Tabs.List>
+	</div>
+	<Tabs.Content value="playlists">
+		<p>playlists</p>
+	</Tabs.Content>
+	<Tabs.Content value="albums">
+		<p>albums</p>
+	</Tabs.Content>
+	<Tabs.Content value="artists">
+		<p>artists</p>
+	</Tabs.Content>
+</Tabs.Root>
