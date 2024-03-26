1c8ad17716dbc5ae1317acb0346adf6c39ec67ca
Thibaud Brault <thibaud.brault@protonmail.com>
2024-03-26 19:58:15 +0100

    :sparkles: create playlist when user is created

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,35 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import google from '@auth/sveltekit/providers/google';
-import { db } from '$lib/db';
+import { db, playlists, users } from '$lib/db';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { eq } from 'drizzle-orm';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [
 		google({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET }),
 	],
-	adapter: DrizzleAdapter(db),
 	trustHost: true,
+	adapter: DrizzleAdapter(db),
+	callbacks: {
+		async signIn({ user }) {
+			const userId = user.id;
+			if (!userId) {
+				return false;
+			}
+			const userExists = await db.query.users.findFirst({
+				where: eq(users.id, userId),
+			});
+			if (!userExists) {
+				await db
+					.insert(playlists)
+					.values({
+						name: 'liked songs',
+						userId,
+					})
+					.execute();
+			}
+			return true;
+		},
+	},
 });
