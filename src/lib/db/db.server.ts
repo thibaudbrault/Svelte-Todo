62663e61c72487b73b2fc8cc37b7c49ffef2a8d6
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-27 17:15:48 +0100

    :wrench: disable prepare

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -3,5 +3,5 @@ import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const client = postgres(DATABASE_URL);
+const client = postgres(DATABASE_URL, { prepare: false });
 export const db = drizzle(client, { schema });
