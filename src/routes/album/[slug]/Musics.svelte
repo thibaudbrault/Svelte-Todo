1b14ad40f5f426f88a13b4a6f093f604954b73ce
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-28 11:31:48 +0100

    :art: create separate component for create music form

--- src/routes/album/[slug]/Musics.svelte
+++ src/routes/album/[slug]/Musics.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import {
-		Button,
-		Dialog,
-		FileInput,
-		Form,
-		NumberInput,
-		ScrollArea,
-		TextInput,
-	} from '$components';
+	import { Button, ScrollArea } from '$components';
 	import { trackId } from '$lib/store';
 	import { format } from '$lib/utils';
-	import { creatOneMusicSchema, createManyMusicSchema } from '$lib/validation';
-	import { Dialog as BitsDialog, Separator, Tabs } from 'bits-ui';
+	import { AddMusic } from '$modules';
+	import { Separator } from 'bits-ui';
 	import { Heart, MoreHorizontal } from 'lucide-svelte';
 
 	export let selectedTrack: number | null;
@@ -21,8 +13,6 @@
 	$: {
 		selectedTrack = $trackId;
 	}
-
-	const acceptedExtensions = '.flac, .mp3';
 
 	let favorites: number[] = [];
 
@@ -86,69 +76,5 @@
 			No music
 		</p>
 	{/if}
-	<Dialog title="New music" trigger="Add music">
-		<BitsDialog.Trigger
-			slot="trigger"
-			class="!mb-16 w-full rounded-md bg-transparent px-4 py-2 text-gray-12 shadow-sm shadow-grayA-7 hover:shadow-grayA-8"
-		>
-			<span class="font-semibold">Add Music</span>
-		</BitsDialog.Trigger>
-		<Tabs.Root value="single" slot="content" class="w-full">
-			<Tabs.List
-				class="mx-auto mb-2 flex w-3/4 items-center justify-center gap-4 rounded-md bg-gray-12 px-4 py-2 text-gray-1"
-			>
-				<Tabs.Trigger
-					value="single"
-					class="rounded-md px-2 py-1 text-xl font-semibold lowercase data-[state=active]:bg-gray-3 data-[state=active]:text-gray-12"
-					style="font-variant: small-caps;">Single</Tabs.Trigger
-				>
-				<Tabs.Trigger
-					value="multiple"
-					class="rounded-md px-2 py-1 text-xl font-semibold lowercase data-[state=active]:bg-gray-3 data-[state=active]:text-gray-12"
-					style="font-variant: small-caps;">Multiple</Tabs.Trigger
-				>
-			</Tabs.List>
-			<Tabs.Content value="single">
-				<Form
-					action="?/creatOneMusic"
-					data={$page.data.formSingle}
-					method="POST"
-					schema={creatOneMusicSchema}
-					class="w-full space-y-4"
-					let:form
-				>
-					<fieldset class="flex w-full flex-col gap-2">
-						<TextInput {form} field="name" label="Name" />
-						<NumberInput {form} field="number" label="Number" min="1" />
-						<FileInput
-							{form}
-							field="track"
-							label="Track"
-							accept={acceptedExtensions}
-						/>
-					</fieldset>
-				</Form>
-			</Tabs.Content>
-			<Tabs.Content value="multiple">
-				<Form
-					action="?/createManyMusic"
-					data={$page.data.formMultiple}
-					method="POST"
-					schema={createManyMusicSchema}
-					class="w-full space-y-4"
-					let:form
-				>
-					<fieldset class="flex w-full flex-col gap-2">
-						<FileInput
-							{form}
-							isMultiple={true}
-							field="tracks"
-							label="Tracks"
-							accept={acceptedExtensions}
-						/>
-					</fieldset>
-				</Form>
-			</Tabs.Content>
-		</Tabs.Root>
-	</Dialog>
+	<AddMusic />
 </div>
