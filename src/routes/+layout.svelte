4853ae5f55f179187c48caf2851ea30b6e25debe
Thibaud BRAULT <thibaud.brault@gide.net>
2024-03-28 16:42:28 +0100

    :heavy_plus_sign: install svelte-sonner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { Toaster } from 'svelte-sonner';
 	import '../app.css';
 	import Navbar from './Navbar.svelte';
 	import Sidebar from './Sidebar.svelte';
 </script>
 
+<Toaster
+	richColors
+	position="top-center"
+	toastOptions={{ style: 'font-weight: 600; font-size: 1.25rem;' }}
+/>
 <main
 	class="relative grid h-screen max-h-screen grid-cols-6 grid-rows-[80px_1fr_8px] gap-y-4 overflow-y-hidden bg-gray-1 p-4 text-gray-12"
 >
