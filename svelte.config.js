56ad76dbeac9e079fa212fc3c32efc02ab738b0f
Thibaud BRAULT <thibaud.brault@gide.net>
2024-04-02 10:27:10 +0200

    :recycle: replace cloudlfare adapter with vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
+		adapter: adapter(),
 		alias: {
 			$components: 'src/components',
 			$modules: 'src/modules',
