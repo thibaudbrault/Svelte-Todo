8f5a3371aef620d518365491de3c392c7ee6c9f2
Thibaud Brault <thibaud.brault@protonmail.com>
2024-04-03 20:24:38 +0200

    :fire: remove top navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Toaster } from 'svelte-sonner';
 	import '../app.css';
-	import Navbar from './Navbar.svelte';
 	import Sidebar from './Sidebar.svelte';
 </script>
 
@@ -10,13 +9,10 @@
 	position="top-center"
 	toastOptions={{ style: 'font-weight: 600; font-size: 1.25rem;' }}
 />
-<main
-	class="relative grid h-screen max-h-screen grid-cols-6 grid-rows-[80px_1fr] gap-y-4 overflow-y-hidden bg-gray-1 p-4 text-gray-12"
->
+<main class="relative flex h-screen gap-y-4 bg-gray-1 p-4 text-gray-12">
 	<Sidebar />
-	<Navbar />
 	<section
-		class="col-span-5 col-start-2 row-span-1 h-full space-y-8 rounded-md border border-gray-5 bg-gray-2 p-4"
+		class="h-full w-full flex-1 overflow-y-auto rounded-md border border-gray-5 bg-gray-2 p-4"
 	>
 		<slot />
 	</section>
