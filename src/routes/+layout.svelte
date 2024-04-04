b4d2096ad5289f5e7d8ce50639a5afe6f20b7b85
Thibaud BRAULT <thibaud.brault@gide.net>
2024-04-04 10:36:52 +0200

    :heavy_plus_sign: install vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import { Toaster } from 'svelte-sonner';
 	import '../app.css';
 	import Sidebar from './Sidebar.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Toaster
