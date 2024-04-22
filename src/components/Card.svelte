aca1af361f75535e474c98f2d6fa4ec5831e3aab
Thibaud BRAULT <thibaud.brault@gide.net>
2024-04-22 19:58:23 +0200

    :lipstick: small ui change

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,7 +8,7 @@
 <div
 	class="flex flex-col justify-center rounded-md border border-gray-5 bg-gray-3 transition-all duration-300 ease-in-out hover:bg-gray-4"
 >
-	<img {alt} src={cover} class="h-40 w-40 rounded-t-md" />
+	<img {alt} src={cover} class="size-44 rounded-t-md" />
 	<h3 class="px-4 py-2 text-center text-sm font-semibold">
 		<a href={link}>{title}</a>
 	</h3>
