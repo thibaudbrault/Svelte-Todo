62d9782938e18a6e1f16c9d8af8f094627d272b0
Thibaud BRAULT <thibaud.brault@gide.net>
2024-04-25 16:37:05 +0200

    :truck: move files from forsmf older to dialogs folder

--- src/modules/forms/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AddAlbumF } from './AddAlbum.svelte';
-export { default as AddGameF } from './AddGame.svelte';
-export { default as AddCompanyF } from './AddCompany.svelte';
-export { default as AddMusicF } from './AddMusic.svelte';
-export { default as DeleteMusicsF } from './DeleteMusics.svelte';
