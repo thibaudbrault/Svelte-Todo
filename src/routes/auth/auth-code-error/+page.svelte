47dbc9e3e97e2a2d936dae548923e165d28dfaef
Thibaud BRAULT <thibaud.brault@gide.net>
2024-05-16 17:29:30 +0200

    :fire: remove auth-code-error page

--- src/routes/auth/auth-code-error/+page.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="w-screen p-4 pt-40 lg:px-16">Auth Code error</div>
--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -7,14 +7,12 @@ export const GET = async (event) => {
 	} = event;
 	const code = url.searchParams.get('code') as string;
 	const next = url.searchParams.get('next') ?? '/';
-
 	if (code) {
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
 			throw redirect(303, `/${next.slice(1)}`);
 		}
+		console.error(error)
 	}
-
-	// return the user to an error page with instructions
-	throw redirect(303, '/auth/auth-code-error');
+	throw redirect(303, '/');
 };
