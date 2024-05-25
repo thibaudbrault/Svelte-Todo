c8347100ce535bbd4ff7000d3c1335307b90d519
Thibaud Brault <thibaud.brault@protonmail.com>
2024-05-25 21:18:01 +0200

    :sparkles: create tooltip component

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Tooltip } from 'bits-ui';
+</script>
+
+<Tooltip.Root>
+	<Tooltip.Trigger>
+		<slot name="trigger" />
+	</Tooltip.Trigger>
+	<Tooltip.Content class="rounded-md border border-gray-6 bg-gray-1 px-2 py-1">
+		<slot name="content" />
+		<Tooltip.Arrow class="border-l border-t border-l-gray-6 border-t-gray-6" />
+	</Tooltip.Content>
+</Tooltip.Root>
--- src/components/index.ts
+++ src/components/index.ts
@@ -9,3 +9,4 @@ export { default as Label } from './Label.svelte';
 export { default as NumberInput } from './NumberInput.svelte';
 export { default as Search } from './Search.svelte';
 export { default as TextInput } from './TextInput.svelte';
+export { default as Tooltip } from './Tooltip.svelte';
