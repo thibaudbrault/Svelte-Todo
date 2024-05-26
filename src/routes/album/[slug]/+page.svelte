532a4c3f55ccde1859401bfcb7c4ec1739342b39
Thibaud Brault <thibaud.brault@protonmail.com>
2024-05-26 17:23:42 +0200

    :sparkles: add search bar in album page

--- src/routes/album/[slug]/+page.svelte
+++ src/routes/album/[slug]/+page.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { SelectMusic } from '$lib/db';
-	import { cover, favoritesMusics, length, musics } from '$lib/store';
+	import { cover, favoritesMusics, length, musics, trackId } from '$lib/store';
 	import { Musics } from '$modules';
 	import { Separator } from 'bits-ui';
 	import { onMount } from 'svelte';
 	import Header from './Header.svelte';
+	import { Search } from '$components';
+	import { debounce } from '$lib/utils';
 
-	$: $musics = $page.data.musics;
+	let query: string = '';
+	let filteredMusics = [];
+
+	$: if (filteredMusics.length > 0) {
+		musics.set(filteredMusics);
+	} else {
+		musics.set($page.data.musics);
+	}
+
 	$: $length = $page.data.length;
+
+	const search = () => {
+		filteredMusics = $page.data.musics.filter((music) => {
+			let musicName = music.name.toLowerCase();
+			return musicName.includes(query.toLowerCase());
+		});
+	};
 
 	onMount(() => {
 		$cover = $page.data.album.cover;
+		$trackId = 0;
 		$page.data.favoritesMusics.forEach((music: SelectMusic) => {
 			favoritesMusics.update((current) => current.add(music.id));
 		});
@@ -21,5 +39,12 @@
 <div class="flex flex-col gap-4">
 	<Header />
 	<Separator.Root class="mx-auto h-px w-11/12 bg-gray-5" />
-	<Musics />
+	<Search bind:query on:input={debounce(search)} placeholder="Search title" />
+	{#if query && filteredMusics.length === 0}
+		<p class="mt-8 flex items-center justify-center text-4xl font-semibold">
+			No Results
+		</p>
+	{:else}
+		<Musics />
+	{/if}
 </div>
