388bab28b445200ad0cc697a0b579aa5d8d3e4cc
Thibaud Brault <thibaud.brault@protonmail.com>
2024-07-21 20:33:18 +0200

    :lipstick: add new text + image

--- src/routes/library/Playlists.svelte
+++ src/routes/library/Playlists.svelte
@@ -12,9 +12,23 @@
 					class="group flex items-center justify-between gap-1 rounded-md p-2 hover:bg-gray-4"
 					href={`/playlist/${playlist.name}`}
 				>
-					<span class="text-2xl font-semibold group-hover:text-yellow-12">
-						{playlist.name}
-					</span>
+					<div class="flex items-center gap-4">
+						<img
+							src={playlist.musics[0].album.cover}
+							class="rounded-md"
+							width="52"
+							height="52"
+							alt={playlist.musics[0].album.name}
+						/>
+						<div class="space-x-2">
+							<span class="text-2xl font-semibold group-hover:text-yellow-12">
+								{playlist.name}
+							</span>
+							<small class="text-xs text-gray-11">
+								{playlist.musics.length} tracks
+							</small>
+						</div>
+					</div>
 					<form>
 						<button class="hover:text-red-9">
 							<X class="size-5" />
--- src/routes/profile/History.svelte
+++ src/routes/profile/History.svelte
@@ -2,8 +2,12 @@
 	import { page } from '$app/stores';
 </script>
 
-<div>
+<div class="space-y-2">
 	{#each $page.data.history as history}
-		<p>{history.music.name}</p>
+		<div class="group rounded-md p-2 hover:bg-gray-4">
+			<p class="text-xl group-hover:text-yellow-12">
+				{history.music.name}
+			</p>
+		</div>
 	{/each}
 </div>
