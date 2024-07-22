ac98df4202039819b7e8b8f7802f5205250e981e
Thibaud Brault <thibaud.brault@protonmail.com>
2024-07-22 21:50:14 +0200

    :bug: fix import of env variables

--- src/lib/utils/website.ts
+++ src/lib/utils/website.ts
@@ -1,3 +1,9 @@
+import {
+	PUBLIC_CONTACT_EMAIL,
+	PUBLIC_GITHUB_PAGE,
+	PUBLIC_SITE_URL,
+} from '$env/static/public';
+
 export const website = {
 	author: 'Thibaud Brault',
 	ogLanguage: 'en_US',
@@ -5,9 +11,9 @@ export const website = {
 	siteTitle: 'GameMusic',
 	siteShortTitle: 'GameMusic',
 	description: 'GameMusic - Listen to your favourite video games soundtracks',
-	siteUrl: process.env.PUBLIC_SITE_URL,
+	siteUrl: PUBLIC_SITE_URL,
 	backgroundColor: '#1b4079',
 	themeColor: '#d62828',
-	contactEmail: process.env.PUBLIC_CONTACT_EMAIL,
-	githubPage: process.env.PUBLIC_GITHUB_PAGE,
+	contactEmail: PUBLIC_CONTACT_EMAIL,
+	githubPage: PUBLIC_GITHUB_PAGE,
 };
