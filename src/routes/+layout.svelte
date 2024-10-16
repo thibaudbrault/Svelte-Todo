a5167b4f96f25edb31270c0fdd5327a96617373f
Thibaud BRAULT <thibaud.brault@gide.net>
2024-10-16 15:22:47 +0200

    :children_crossing: create navbar for closed drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,25 @@
 	import { goto, invalidate } from '$app/navigation';
 	import {
 		audio,
+		authors,
 		cover,
 		currentTime,
 		duration,
 		isLoading,
 		isPlayerOpen,
+		isPlaying,
 		isZen,
+		title,
 	} from '$lib/store';
-	import { nextTrack } from '$lib/utils';
+	import { nextTrack, playPauseTrack, truncate } from '$lib/utils';
 	import { Player, PlayerMobile } from '$modules';
 	import { inject } from '@vercel/analytics';
 	import { onMount } from 'svelte';
 	import { Toaster } from 'svelte-sonner';
 	import '../app.css';
 	import Sidebar from './Sidebar.svelte';
+	import { Button } from '$components';
+	import { Loader2Icon, Pause, Play } from 'lucide-svelte';
 
 	inject({ mode: dev ? 'development' : 'production' });
 
@@ -84,5 +89,49 @@
 		{:else}
 			<PlayerMobile {raf} />
 		{/if}
+	{/if}
+	{#if !$isPlayerOpen && $audio?.src && innerWidth < 768}
+		<section
+			role="button"
+			tabindex="-1"
+			on:keypress={(event) => event.key === 'Enter' && ($isPlayerOpen = true)}
+			on:click={() => {
+				$isPlayerOpen = true;
+			}}
+			class="fixed bottom-20 left-1 right-1 flex items-center justify-between rounded-md border border-yellow-6 bg-gray-1 px-6 py-2"
+		>
+			<div class="flex items-center gap-2">
+				<img src={$cover} alt="" class="mx-auto max-w-12 rounded-md" />
+				<div class="flex flex-col justify-start gap-1">
+					<p>
+						{truncate($title, 15)}
+					</p>
+					<div class="flex items-center gap-1">
+						{#each $authors as authors}
+							<p class="text-xs">{authors.author.name}</p>
+						{/each}
+					</div>
+				</div>
+			</div>
+			<Button
+				aria-label={$isPlaying ? 'Pause' : 'Play'}
+				intent="primary"
+				rounded="full"
+				size="icon"
+				on:click={(event) => {
+					event.stopPropagation();
+					playPauseTrack(raf);
+				}}
+				disabled={$isLoading}
+			>
+				{#if $isLoading}
+					<Loader2Icon class="size-5 animate-spin" />
+				{:else if $isPlaying}
+					<Pause class="size-5" />
+				{:else}
+					<Play class="size-5" />
+				{/if}
+			</Button>
+		</section>
 	{/if}
 </main>
